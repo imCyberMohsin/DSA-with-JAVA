bcceb80d29e976b0b3d0414dfe8eb900dc3dfec5
imCyberMohsin <mohsinansari7033@gmail.com>
2024-07-22 22:35:04 +0530

    tcs sheet

--- Z_TCS_NQT_Sheet_Solutions/A_Arrays/C_.java
+++ Z_TCS_NQT_Sheet_Solutions/A_Arrays/C_.java
@@ -1,12 +1,11 @@
 //! Find Second Smallest and Second Largest Element in an array
 
-
 public class C_ {
     public static void main(String[] args) {
-        
+
     }
 
-    static void secondLargest(int arr[]){
+    static void secondLargest(int arr[]) {
         int Largest = arr[0];
 
     }
