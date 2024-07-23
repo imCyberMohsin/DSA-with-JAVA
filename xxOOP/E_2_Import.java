ba779e1fbebbf8ba986255b33cc692c6fa1acf15
imCyberMohsin <mohsinansari7033@gmail.com>
2024-07-23 21:38:29 +0530

    oop done

---
+++ xxOOP/E_2_Import.java
@@ -0,0 +1,8 @@
+import bank;
+
+public class E_2_Import {
+    public static void main(String[] args) {
+        bank.Account = new bank.Account();
+        account1.name = "Mohsin's Bank";
+    }
+}
