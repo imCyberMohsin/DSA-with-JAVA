14141364adc03acfa7877e1ef8b450f48cb672bd
imCyberMohsin <mohsinansari7033@gmail.com>
2024-07-30 17:52:54 +0530

    linear search done

--- F_Arrays/D_Searching/A_Linear_search.java
+++ F_Arrays/D_Searching/A_Linear_search.java
@@ -1,3 +1,24 @@
+//! Linear Search in 1D Array
+// Return the index of element
+
 public class A_Linear_search {
-    
+    public static void main(String[] args) {
+        int arr[] = { 1, 2, 3, 4, 5, 6 };
+        int num = 5;    // index: 4
+        int n = arr.length;
+
+        System.out.println(linearSearch(arr, num, n));
+    }
+
+    //? Function
+    static int linearSearch(int arr[], int num, int n) {
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == num) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    //? Time complexity: O(n), one loop running from 0 to n which is the no. elements in the array
+    //? Space complexity: O(1), no extra space is taken. 
 }
--- F_Arrays/D_Searching/B_Linear_search_in_2D_Array.java
+++ F_Arrays/D_Searching/B_Linear_search_in_2D_Array.java
@@ -1,3 +1,31 @@
+//! Linear search in 2D Arrays 
+
 public class B_Linear_search_in_2D_Array {
-    
+    public static void main(String[] args) {
+        int arr[][] = {
+            {1, 2, 3, 4, 5},
+            {6, 6, 7, 8, 9},
+            {10, 11, 12, 13}
+        };
+        int num = 2;
+
+        int result[] = linearSerach2Darray(arr, num);
+        for(int i: result){
+            System.out.print("["+ i + "]");
+        }
+    }
+
+    // Function
+    static int[] linearSerach2Darray(int arr[][], int num) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if (arr[i][j] == num) {
+                    return new int[]{i,j};
+                }
+            }
+        }
+        return new int[]{-1, -1};
+    }
+    //? Time complexity: O(n * m)
+    //? Space complexity: O(1) 
 }
